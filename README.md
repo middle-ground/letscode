3bd9495f8fb410ad00e6611ca89d3dddecb33209
Ayu Sudi <ayusudi.abc@gmail.com>
2023-06-08 23:23:00 +0700

    listing the main feature

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 sequelize model:create --name User --attributes username:string,email:string,password:string,linkedIn:string,verifiedStatus:boolean,onlineStatus:boolean
 sequelize model:create --name Interest --attributes name:string,related_to:string 
+
+- Register
+- Login
+- Read profile
+- Choose Interest 
+- Update Interest
+- Connect LinkedIn*
+- Verified Email*
+- Email Cron Job*
+- List springer & search*
+- Read One Springer*
+- Create Clippings
+- Add Link to Clippings
+- Read Clippings (public)
+- Read Clippings (owned or participate)
+- Read One Clippings
+- Add Friend to Clipping
+- Follow friends
+- Timeline
+- Logout
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,19 @@
 const router = require("express").Router()
 
+router.get("/", (req, res) => res.send("hello world"))
+router.get("/register", (req, res) => res.send("hello world"))
+router.post("/register", (req, res) => res.send("hello world"))
+router.get("/login", (req, res) => res.send("hello world"))
+router.post("/login", (req, res) => res.send("hello world"))
+router.post("/profile/interests", (req, res) => res.send("hello world"))
+router.get("/profile", (req, res) => res.send("hello world"))
+router.get("/profile/linkedin", (req, res) => res.send("hello world"))
+router.post("/clippings", (req, res) => res.send("hello world"))
+router.post("/clippings/owned", (req, res) => res.send("hello world"))
+router.post("/clippings/:id", (req, res) => res.send("hello world"))
+router.post("/clippings/:id/friends/:username", (req, res) => res.send("hello world"))
+router.get("/clippings/@/:username", (req, res) => res.send("hello world"))
+router.get("/followers/:username", (req, res) => res.send("hello world"))
+router.post("/followers/:username", (req, res) => res.send("hello world"))
+
 module.exports = router
